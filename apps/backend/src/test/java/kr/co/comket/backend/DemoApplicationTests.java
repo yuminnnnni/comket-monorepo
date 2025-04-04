3c7d235fcb2a47e4c00e599cdd9b01124d36bf29
Jayfunf <simh3077@gmail.com>
2025-04-04 16:52:04 +0900

    Del - 데모 테스트 파일 삭제

--- apps/backend/src/test/java/kr/co/comket/backend/DemoApplicationTests.java
+++ apps/backend/src/test/java/kr/co/comket/backend/DemoApplicationTests.java
@@ -1,13 +1,13 @@
-package kr.co.comket.backend;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package kr.co.comket.backend;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DemoApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
