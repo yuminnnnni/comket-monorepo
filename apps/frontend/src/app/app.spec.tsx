c0968d53ceff871222b47dda1b2e43d275d76428
yuminnnnni <ka09023@ajou.ac.kr>
2025-04-06 02:44:12 +0900

    test: UI 텍스트 변경에 맞춰 App 테스트 수정

--- apps/frontend/src/app/app.spec.tsx
+++ apps/frontend/src/app/app.spec.tsx
@@ -20,7 +20,7 @@ describe('App', () => {
       </BrowserRouter>
     );
     expect(
-      getAllByText(new RegExp('Welcome frontend', 'gi')).length > 0
+      getAllByText(/Welcome/i).length > 0
     ).toBeTruthy();
   });
 });
