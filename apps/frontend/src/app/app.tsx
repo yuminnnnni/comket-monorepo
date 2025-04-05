53b192c2a316f5b4d9f3e4167e5ade2dcfe72dd8
yuminnnnni <ka09023@ajou.ac.kr>
2025-04-06 02:37:06 +0900

    chore: update app.tsx for deployment test

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -10,18 +10,12 @@ const StyledApp = styled.div`
 export function App() {
   return (
     <StyledApp>
-      <NxWelcome title="frontend" />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
       <hr />
       <br />
       <div role="navigation">
         <ul>
           <li>
-            <Link to="/">Welcome to COMKET!!!</Link>
+            <Link to="/">Welcome to COMKET!!! Updated</Link>
           </li>
           <li>
             <Link to="/page-2">Page 2</Link>
