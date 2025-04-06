2af245df39c0251c9ae6e0a054134f5614a83dbe
yuminnnnni <ka09023@ajou.ac.kr>
2025-04-06 15:49:35 +0900

    fix: Vercel 빌드 환경에서 Nx 설치

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -15,7 +15,7 @@ export function App() {
       <div role="navigation">
         <ul>
           <li>
-            <Link to="/">Welcome to COMKET!!! Updated🚨</Link>
+            <Link to="/">Welcome to COMKET!!! Updated🚨🚨</Link>
           </li>
           <li>
             <Link to="/page-2">Page 2</Link>
--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@ set -e  # 에러 발생 시 스크립트 중단
 
 pnpm install
 
+# Install Nx explicitly if it's not installed
+pnpm add -D nx
+
 # Check if Nx is correctly installed
 echo "NX PATH: $(which nx)"
 
